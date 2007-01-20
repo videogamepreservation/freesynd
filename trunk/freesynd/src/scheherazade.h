76a514dab078f6d1588c098e573a4b532ea09725
gargoyle <gargoyle@3ce38193-f967-438d-acd5-fc4e68e0da95>
2007-01-20 11:57:02 +0000

    Blasting old headers.

--- trunk/freesynd/src/scheherazade.h
+++
@@ -1,7 +0,0 @@
-
-int fli_new(struct lua_State *L);
-int fli_gc(struct lua_State *L);
-int fli_play(struct lua_State *L);
-int fli_rewind(struct lua_State *L);
-
-int init_fliplayer(void);
--- trunk/freesynd/src/uenuku.h
+++
@@ -1,3 +0,0 @@
-
-struct SDL_Color *get_pal(const char *n);
-int init_uenuku(void);
